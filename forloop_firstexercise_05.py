ad814ae6fa113611aa14e38c9bea5eaa7446373e
Nathaniel Godwin <168899179+natodango@users.noreply.github.com>
2026-01-01 10:15:31 +0100

    Create forloop_firstexercise_05.py

---
+++ forloop_firstexercise_05.py
@@ -0,0 +1,7 @@
+fruits =["apple","watermelon","orange","mango"]
+
+for fruit in fruits:
+    print(fruit)
+    print(f"Deeplearn.{fruit}@gmail.com")
+    
+print("\nWelcome to 2026🎊 and congratulation to all the above listed details")
